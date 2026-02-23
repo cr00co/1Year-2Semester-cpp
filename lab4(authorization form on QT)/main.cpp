5d5164992a691b07288bab1fd4e961456c43fc80
Anton <antonpuh1162@gmail.com>
2026-02-23 19:19:43 +0200

    Revert "Add Qt authorization form and user DB"
    This reverts commit 410acb44bc3cd6a9d60d6a1ee1cef18fa3f8b5f4.

--- lab4(authorization form on QT)/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "AuthorizationForm.hpp"
-
-int main(int argc, char* argv[]) {
-    QApplication app(argc, argv);
-    AuthorizationForm window;
-    window.show();
-    return app.exec();
-}
